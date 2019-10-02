89d55fa9016e3e2412ea87afc7846a9f66f52a9c
OPSMinistryEducation <54960138+OPSMinistryEducation@users.noreply.github.com>
2019-10-02 16:48:39 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+
+// import { sign } from "tweetnacl";
+// import { decodeUTF8, encodeUTF8 } from "tweetnacl-util";
+
 // register collects the information provided in the form and saves it in the
 // local credentials store.
 const register = async () => {
+  const keys = generateKeys()
   const user = {
     email: document.getElementById('email').value.trim(),
     password: document.getElementById('password').value,
+    publicKey: keys.publicKey,
+    privateKey: keys.privateKey
   }
 
   // Persist access information in the local credentials store
   const cred = new PasswordCredential({
     id: user.email,
-    password: user.password
+    password: user.password,
+    publicKey: user.publicKey,
+    privateKey: user.privateKey
   })
 
   await navigator.credentials.store(cred)
   show(`Credentials stored for ${user.email}`)
+  show(`User's generated public key ${user.publicKey}`)
 }
+
 
 const login = async () => {
   const cred = await navigator.credentials.get({password: true})
@@ -28,4 +39,16 @@ const login = async () => {
 
 const show = (msg) => {
   document.getElementById('welcome-text').innerHTML = msg
+}
+
+
+function generateKeys() {
+  return tweetnacl.sign.keyPair()
+}
+
+
+function generateSignature(message, privateKey){
+  const bytes = decodeUTF8(message);
+  const signature = sign.detached(bytes, privateKey);
+  return signature
 }
